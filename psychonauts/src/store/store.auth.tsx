5339e76ff49553785de193c44dea37e5769f4518
mirosteiger <52956540+mirosteiger@users.noreply.github.com>
2023-07-13 14:31:50 +0200

    Update store.auth.tsx

--- psychonauts/src/store/store.auth.tsx
+++ psychonauts/src/store/store.auth.tsx
@@ -1,49 +1,52 @@
 import { create } from "zustand"
-import { persist } from "zustand/middleware"
+import { devtools, persist } from "zustand/middleware"
 import { auth } from "database/firebaseConfigSetter"
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth"
 import { IAuthStore, ICredentials } from "types"
 
 export const useAuthStore = create<IAuthStore>()(
-    persist(
-        (set) => ({
-            isLoggedIn: auth.currentUser !== null,
-            loginError: '',
-            currentUser: undefined,
-            login: async (cred: ICredentials) => {
-                const { email, password } = cred
-                await signInWithEmailAndPassword(auth, email, password)
-                    .then((userCredentials) => {
-                        set({ currentUser: userCredentials.user })
-                        set({ isLoggedIn: true })
-                    })
-                    .catch((e) => {
-                        console.log(e)
-                    })
-            },
-            logout: () => {
-                signOut(auth)
-                    .then(() => {
-                        console.log("signed out")
-                        set({ isLoggedIn: false })
-                    })
-                    .catch(err => {
-                        console.log(err)
-                    })
-            },
+    devtools(
+        persist(
+            (set) => ({
+                isLoggedIn: auth.currentUser !== null,
+                loginError: '',
+                currentUser: undefined,
+                login: async (cred: ICredentials) => {
+                    const { email, password } = cred
+                    await signInWithEmailAndPassword(auth, email, password)
+                        .then((userCredentials) => {
+                            set({ currentUser: userCredentials.user })
+                            set({ isLoggedIn: true })
+                        })
+                        .catch((e) => {
+                            console.log(e)
+                        })
+                },
+                logout: () => {
+                    signOut(auth)
+                        .then(() => {
+                            console.log("signed out")
+                            set({ isLoggedIn: false })
+                        })
+                        .catch(err => {
+                            console.log(err)
+                        })
+                },
 
-            register: async (cred: ICredentials) => {
-                const { email, password } = cred
-                await createUserWithEmailAndPassword(auth, email, password)
-                    .then((userCredentials) => {
-                        set({ currentUser: userCredentials.user })
-                        set({ isLoggedIn: true })
-                    })
-                    .catch((e) => {
-                        console.log(e)
-                    });
+                register: async (cred: ICredentials) => {
+                    const { email, password } = cred
+                    await createUserWithEmailAndPassword(auth, email, password)
+                        .then((userCredentials) => {
+                            set({ currentUser: userCredentials.user })
+                            set({ isLoggedIn: true })
+                        })
+                        .catch((e) => {
+                            console.log(e)
+                        });
 
-            }
-        }),
-        { name: "user" }),
+                }
+            }),
+            { name: "user" }),
+    )
+
 )
