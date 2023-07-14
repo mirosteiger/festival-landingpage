9fb45cecbeee8ef7b556bb8335b86945b8c5ecf5
mirosteiger <52956540+mirosteiger@users.noreply.github.com>
2023-07-14 11:58:45 +0200

    remove currentUser after logout

--- psychonauts/src/store/store.auth.tsx
+++ psychonauts/src/store/store.auth.tsx
@@ -1,8 +1,9 @@
 import { create } from "zustand"
 import { devtools, persist } from "zustand/middleware"
-import { auth } from "database/firebaseConfigSetter"
+import { auth } from "database/firebase.configSetter"
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth"
 import { IAuthStore, ICredentials } from "types"
+import { createUserCart } from "database/firebase.utils"
 
 export const useAuthStore = create<IAuthStore>()(
     devtools(
@@ -10,13 +11,14 @@ export const useAuthStore = create<IAuthStore>()(
             (set) => ({
                 isLoggedIn: auth.currentUser !== null,
                 loginError: '',
-                currentUser: undefined,
+                currentUser: null,
                 login: async (cred: ICredentials) => {
                     const { email, password } = cred
                     await signInWithEmailAndPassword(auth, email, password)
                         .then((userCredentials) => {
                             set({ currentUser: userCredentials.user })
                             set({ isLoggedIn: true })
+                            
                         })
                         .catch((e) => {
                             console.log(e)
@@ -27,6 +29,8 @@ export const useAuthStore = create<IAuthStore>()(
                         .then(() => {
                             console.log("signed out")
                             set({ isLoggedIn: false })
+                            set({ currentUser: null })
+                            
                         })
                         .catch(err => {
                             console.log(err)
@@ -39,6 +43,7 @@ export const useAuthStore = create<IAuthStore>()(
                         .then((userCredentials) => {
                             set({ currentUser: userCredentials.user })
                             set({ isLoggedIn: true })
+                            createUserCart(userCredentials.user.uid)
                         })
                         .catch((e) => {
                             console.log(e)
